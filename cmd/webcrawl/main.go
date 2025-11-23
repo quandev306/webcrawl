f4d06348c3020db57b5c4b22d76893b7e674adb4
Quan Le <quan.dev.30.06.2001@gmail.com>
2025-11-23 13:06:38 +0700

    update

--- cmd/webcrawl/main.go
+++ cmd/webcrawl/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-  "fmt"
-  route 
+	"log"
+
+route "github.com/quandev306/webcrawl/api/route"
+"github.com/quandev306/webcrawl/bootstrap"
+"github.com/gin-gonic/gin"
 )
 
 func main() {
+	r := gin.Default()
+	r.GET("/healthz", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
 
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
 }
